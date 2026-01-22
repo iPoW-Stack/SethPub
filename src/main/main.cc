d801fccce8003f3c2df6d1d93d1d9aa84f48f5a0
tenondvpn <jhsx5679@gmail.com>
2026-01-22 16:39:03 +0800

    fix

--- src/main/main.cc
+++ src/main/main.cc
@@ -7,9 +7,9 @@
 static void GlobalInitSpdlog() {
     spdlog::init_thread_pool(8192, 1);
 
-    // auto logger = spdlog::create_async<spdlog::sinks::basic_file_sink_mt>(
-    //     "async_file", "log/seth.log", false);
-    auto logger = spdlog::basic_logger_mt("sync_file", "log/seth.log", false);
+    auto logger = spdlog::create_async<spdlog::sinks::basic_file_sink_mt>(
+        "async_file", "log/seth.log", false);
+    // auto logger = spdlog::basic_logger_mt("sync_file", "log/seth.log", false);
     spdlog::set_default_logger(logger);
     spdlog::set_pattern("%Y-%m-%d %H:%M:%S.%e [thread %t] %-5l [%n] %v%$");
     for (auto& sink : logger->sinks()) {
