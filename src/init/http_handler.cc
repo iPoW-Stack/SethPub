1fba733255b84719089f366e4f1ed8d8bf431ac1
tenondvpn <jhsx5679@gmail.com>
2026-01-31 20:54:16 +0800

    fix

--- src/init/http_handler.cc
+++ src/init/http_handler.cc
@@ -1101,7 +1101,7 @@ static void GetBlockWithHash(const httplib::Request& req, httplib::Response& htt
     }
 
     auto hash_list = req_json["hash_list"];
-    int23_t count = 0;
+    int32_t count = 0;
     for (auto& hash_val : hash_list) {
         if (!hash_val.is_string()) {
             continue;
