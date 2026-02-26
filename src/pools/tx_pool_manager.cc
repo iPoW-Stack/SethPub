77b10090555f7c8d004bee17523159e431331914
tenondvpn <jhsx5679@gmail.com>
2026-02-26 15:29:24 +0800

    fix

--- src/pools/tx_pool_manager.cc
+++ src/pools/tx_pool_manager.cc
@@ -572,6 +572,10 @@ void TxPoolManager::HandlePoolsMessage(const transport::MessagePtr& msg_ptr) {
             pool_index = msg_ptr->address_info->pool_index();
             break;
         }
+        case pools::protobuf::kStatistic: {
+            pool_index = msg_ptr->address_info->pool_index();
+            break;
+        }
         default:
             SETH_DEBUG("invalid tx step: %d", (int32_t)tx_msg.step());
             assert(false);
