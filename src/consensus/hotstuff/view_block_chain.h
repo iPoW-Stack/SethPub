a6954cef0bf79bcf53c94d7fcfae1b2fac8e9809
tenondvpn <jhsx5679@gmail.com>
2026-03-03 16:40:25 +0800

    fix

--- src/consensus/hotstuff/view_block_chain.h
+++ src/consensus/hotstuff/view_block_chain.h
@@ -203,15 +203,15 @@ public:
                 high_view_block_->block_info().height());
         }
 
-        auto pre_block = Get(high_view_block_->parent_hash());
-        if (pre_block && pre_block->view_block) {
-            SETH_DEBUG("pool: %d, check pool chain is full, pre block height: %lu, high view block height: %lu", 
-                pool_index_, pre_block->view_block->block_info().height(), high_view_block_->block_info().height());
-            return pools_mgr_->PoolChainIsFull(
+        if (pools_mgr_->PoolChainIsFull(
                 pool_index_, 
-                pre_block->view_block->block_info().height() - 1);
+                high_view_block_->block_info().height())) {
+            return true;
         }
 
+        SETH_DEBUG("pool: %d, check pool chain is full, high view block height: %lu, latest committed block height: %lu", 
+            pool_index_, high_view_block_->block_info().height(), 
+            latest_committed_block ? latest_committed_block->block_info().height() : 0);
         return pools_mgr_->PoolChainIsFull(
             pool_index_, 
             high_view_block_->block_info().height() - 1);
