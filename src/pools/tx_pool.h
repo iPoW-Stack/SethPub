fe9051c7db4eedb3eaed0eaf2ab7b71494c61cc4
tenondvpn <jhsx5679@gmail.com>
2026-03-03 17:22:05 +0800

    fix

--- src/pools/tx_pool.h
+++ src/pools/tx_pool.h
@@ -69,7 +69,7 @@ public:
     void SyncBlock();
     void TxOver(view_block::protobuf::ViewBlockItem& view_block);
     bool PoolChainIsFull(uint64_t height) const {
-        if (latest_height_ != height) {
+        if (latest_height_ < height) {
             SETH_DEBUG("pool: %d, check pool chain is full height: %lu, latest_height_: %lu", 
                 pool_index_, height, latest_height_);
             return false;
