764a30788c0938cd66034b63996c3e2a4e1ab698
tenondvpn <jhsx5679@gmail.com>
2026-03-08 18:15:19 +0800

    fix

--- src/consensus/hotstuff/view_block_chain.h
+++ src/consensus/hotstuff/view_block_chain.h
@@ -243,6 +243,16 @@ private:
         }
         
         view_blocks_info_[view_block_info->view_block->qc().view_block_hash()] = view_block_info;
+        SETH_DEBUG("store now add view block: %s, %u_%u_%lu, height: %lu, "
+            "parent hash: %s, tx size: %u, strings: %s",
+            common::Encode::HexEncode(view_block_info->view_block->qc().view_block_hash()).c_str(),
+            view_block_info->view_block->qc().network_id(),
+            view_block_info->view_block->qc().pool_index(),
+            view_block_info->view_block->qc().view(),
+            view_block_info->view_block->block_info().height(),
+            common::Encode::HexEncode(view_block_info->view_block->parent_hash()).c_str(),
+            view_block_info->view_block->block_info().tx_list_size(),
+            String().c_str());
         assert(view_with_blocks_.find(view_block_info->view_block->qc().view()) == view_with_blocks_.end());
         view_with_blocks_[view_block_info->view_block->qc().view()] = view_block_info;
         SETH_DEBUG("success add view block info now size: %u", view_blocks_info_.size());
