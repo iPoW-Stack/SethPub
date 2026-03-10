ee5217a314a923a9ea411110dfb53fa41bc98de8
tenondvpn <jhsx5679@gmail.com>
2026-03-10 12:56:17 +0800

    fix

--- src/pools/tx_pool_manager.cc
+++ src/pools/tx_pool_manager.cc
@@ -113,10 +113,11 @@ int TxPoolManager::TmpFirewallCheckMessage(const transport::MessagePtr& msg_ptr)
     auto& tx_msg = header.tx_proto();
     if (!IsUserTransaction(tx_msg.step())) {
         if (!msg_ptr->system_message) {
+            auto addr = security_->GetAddress(tx_msg.pubkey());
             SETH_DEBUG("pools message fierwall coming is system message, "
                 "step: %u, from: %s, to: %s", tx_msg.step(),
-                common::Encode::HexEncode(header.from()).c_str(), 
-                common::Encode::HexEncode(header.to()).c_str());
+                common::Encode::HexEncode(addr).c_str(), 
+                common::Encode::HexEncode(tx_msg.to()).c_str());
             return transport::kFirewallCheckError;
         }
 
