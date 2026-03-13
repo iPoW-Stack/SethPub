d0f3935f27452d35aabe567f0ebf3c414a8626f4
tenondvpn <jhsx5679@gmail.com>
2026-03-13 13:32:41 +0800

    fix build third

--- build_third.sh
+++ build_third.sh
@@ -1,103 +1,138 @@
-#!/bin/bash
-
-# --- 1. System Dependencies ---
-# Check OS and install only if necessary
-if [ -f /etc/debian_version ]; then
-    sudo apt update
-    sudo apt install -y autoconf automake libtool texinfo libgnutls28-dev \
-                        liblzma-dev pkg-config yasm zlib1g-dev libssh2-1-dev cmake
-elif [ -f /etc/redhat-release ]; then
-    sudo dnf install -y gnutls-devel perl procps-ng-devel texinfo xz-devel cmake
-fi
-
 git submodule init
 git submodule update
 
-export nproc=$(nproc 2>/dev/null || echo 8)
+export nproc=8
+#centos
+dnf install -y gnutls-devel
+dnf install -y perl
+dnf install -y procps-ng-devel
+dnf install -y texinfo
+dnf install -y xz-devel
+#ubuntu
+apt update
+sudo apt install autoconf automake libtool -y
+apt install -y libprocps-dev
+apt install -y texinfo
+apt install -y libgnutls28-dev
+apt install -y liblzma-dev
+apt install -y pkg-config
+apt install -y yasm
+apt install -y libgnutls28-dev
+apt install -y zlib1g-dev
+apt install -y libssh2-1-dev
 SRC_PATH=`pwd`
-TP_PATH="$SRC_PATH/third_party"
-mkdir -p $TP_PATH/lib $TP_PATH/include
+cd $SRC_PATH
+cd third_party/evmone && git checkout master &&  git submodule update --init && cmake -S . -B build_release -DCMAKE_POLICY_VERSION_MINIMUM=3.5 -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DCMAKE_INSTALL_PREFIX=$SRC_PATH/third_party/ && cd build_release && make -j${nproc} && make install
+cd $SRC_PATH
+cd third_party/evmone/evmc &&  git submodule update --init && cmake -S . -B build_release -DCMAKE_POLICY_VERSION_MINIMUM=3.5 -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$SRC_PATH/third_party/ && cd build_release && make -j${nproc} && make install
 
-# --- Function to check and build ---
-# Usage: check_and_build "sentinel_file" "directory" "build_commands"
-build_lib() {
-    if [ ! -f "$1" ] && [ ! -d "$1" ]; then
-        echo "Building $2..."
-        cd $SRC_PATH/$2
-        eval "$3"
-    else
-        echo "Skipping $2 (already built)."
-    fi
+if [ ! -d "$SRC_PATH/third_party/include/sodium" ]; then
     cd $SRC_PATH
-}
+    cd third_party/libsodium && git checkout 9511c98 && git submodule update --init && ./configure --prefix=$SRC_PATH/third_party/ && make -j${nproc} && make install 
+fi
 
-# --- 2. Build Components ---
+cd $SRC_PATH
+cd third_party/maxmind/ && git submodule init && git submodule update && cmake -S . -B build_release -DCMAKE_POLICY_VERSION_MINIMUM=3.5 -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$SRC_PATH/third_party/ && cd build_release && make -j${nproc}
+cp -rnf libmaxminddb.a $SRC_PATH/third_party/lib/
+mkdir -p $SRC_PATH/third_party/include/maxmind && cd .. && cp -rnf ./include/* $SRC_PATH/third_party/include/maxmind && cp -rnf build_release/generated/maxminddb_config.h $SRC_PATH/third_party/include/maxmind/
+mkdir -p $SRC_PATH/third_party/include/maxmind/include && cp -rnf ./include/* $SRC_PATH/third_party/include/maxmind/include && cp -rnf build_release/generated/maxminddb_config.h $SRC_PATH/third_party/include/maxmind/include
+if [ ! -d "$SRC_PATH/third_party/include/libuv" ]; then
+    cd $SRC_PATH
+    cd third_party/libuv && rm -rf build_release && git checkout 5152db2 && git submodule init && git submodule update && cmake -S . -B build_release -DCMAKE_POLICY_VERSION_MINIMUM=3.5 -DCMAKE_BUILD_TYPE=$TARGET -DCMAKE_INSTALL_PREFIX=$SRC_PATH/third_party/ && cd build_release && make -j8 && make install
+    rm -rf $SRC_PATH/third_party/include/libuv
+    mv $SRC_PATH/third_party/include/uv $SRC_PATH/third_party/include/libuv
+    sed -i 's/"uv\//"libuv\//g' $SRC_PATH/third_party/include/uv.h
+    sed -i 's/"uv\//"libuv\//g' $SRC_PATH/third_party/include/libuv/unix.h
+    sed -i 's/"uv\//"libuv\//g' $SRC_PATH/third_party/include/libuv/win.h
+fi
+cd $SRC_PATH
+cd third_party/libbls && cd ./deps && PARALLEL_COUNT=1 bash build.sh && cp deps_inst/x86_or_x64/lib64/lib* deps_inst/x86_or_x64/lib/ ; cd .. && cmake -S . -B build_release  -DUSE_ASM=False  -DWITH_PROCPS=OFF -DCMAKE_POLICY_VERSION_MINIMUM=3.5 -DLIBBLS_BUILD_TESTS=OFF -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$SRC_PATH/third_party/ && cd build_release && make -j8 && make install
+mkdir -p $SRC_PATH/third_party/include/libbls && cp -rnf ../third_party ../tools ../dkg ../bls $SRC_PATH/third_party/include/libbls
+cp -rnf ../deps/deps_inst/x86_or_x64/include/boost/* $SRC_PATH/third_party/include/boost/
+cp -rnf ./libbls.a $SRC_PATH/third_party/lib/libdkgbls.a
 
-# Evmone
-build_lib "$TP_PATH/lib/libevmone.a" "third_party/evmone" \
-    "git checkout master && git submodule update --init && cmake -S . -B build_release -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DCMAKE_INSTALL_PREFIX=$TP_PATH && cd build_release && make -j${nproc} install"
+cd $SRC_PATH
+cd third_party/protobuf/ && git checkout 48cb18e && ./autogen.sh && ./configure --disable-shared --enable-static CXXFLAGS="-fPIC -O3" CFLAGS="-fPIC -O3" --prefix=$SRC_PATH/third_party/ && make -j${nproc} && make install
 
-# EVMC
-build_lib "$TP_PATH/include/evmc/evmc.h" "third_party/evmone/evmc" \
-    "git submodule update --init && cmake -S . -B build_release -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$TP_PATH && cd build_release && make -j${nproc} install"
+cd $SRC_PATH
+cd third_party/spdlog && git checkout . && git submodule update --init && cmake -S . -B build_release -DSPDLOG_ENABLE_SOURCE_LOC=ON -DWITH_TESTS=OFF -DPORTABLE=1  -DCMAKE_CXX_FLAGS="-Wno-maybe-uninitialized" -DWITH_GFLAGS=OFF -DCMAKE_POLICY_VERSION_MINIMUM=3.5 -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$SRC_PATH/third_party/ && cd build_release && make -j${nproc} && make install
 
-# Libsodium
-build_lib "$TP_PATH/include/sodium.h" "third_party/libsodium" \
-    "git checkout 9511c98 && git submodule update --init && ./autogen.sh && ./configure --prefix=$TP_PATH && make -j${nproc} install"
+cd $SRC_PATH
+cd third_party/rocksdb && git checkout . && sed -i "s/-march=native//g" ./CMakeLists.txt && git submodule update --init && cmake -S . -B build_release -DWITH_TESTS=OFF -DPORTABLE=1  -DCMAKE_CXX_FLAGS="-Wno-maybe-uninitialized" -DWITH_GFLAGS=OFF -DCMAKE_POLICY_VERSION_MINIMUM=3.5 -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$SRC_PATH/third_party/ && cd build_release && make -j${nproc} && make install
 
-# Maxmind
-build_lib "$TP_PATH/lib/libmaxminddb.a" "third_party/maxmind" \
-    "git submodule update --init && cmake -S . -B build_release -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$TP_PATH && cd build_release && make -j${nproc} && \
-     cp -rnf libmaxminddb.a $TP_PATH/lib/ && mkdir -p $TP_PATH/include/maxmind && cp -rnf ../include/* $TP_PATH/include/maxmind/ && \
-     cp -rnf build_release/generated/maxminddb_config.h $TP_PATH/include/maxmind/"
+cd $SRC_PATH
+cd third_party/log4cpp && git checkout . && sed -i 's/SHARED/STATIC/g' ./CMakeLists.txt &&  sed -i '14i\#include <ctime>' ./include/log4cpp/DailyRollingFileAppender.hh  && cmake -S . -B build_release -DBUILD_SHARED_LIBS=OFF  -DCMAKE_POLICY_VERSION_MINIMUM=3.5 -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$SRC_PATH/third_party/ && cd build_release && make -j${nproc} && make install
 
-# Libuv
-build_lib "$TP_PATH/include/libuv/uv.h" "third_party/libuv" \
-    "git checkout 5152db2 && git submodule update --init && cmake -S . -B build_release -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$TP_PATH && cd build_release && make -j${nproc} install && \
-     mv $TP_PATH/include/uv $TP_PATH/include/libuv && sed -i 's/\"uv\//\"libuv\//g' $TP_PATH/include/uv.h"
+cd $SRC_PATH
+cd third_party/gmssl && git checkout d655c06 && sed -i "s/-march=native//g" ./CMakeLists.txt && sed -i '19i\#include <gmssl/sm2.h>' ./include/gmssl/sm2_recover.h && cmake -S . -B build_release -DBUILD_SHARED_LIBS=OFF -DCMAKE_POLICY_VERSION_MINIMUM=3.5 -DENABLE_SM2_EXTS=on -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$SRC_PATH/third_party/ && cd build_release && make -j${nproc} && make install
+objcopy --localize-symbol=OPENSSL_hexchar2int        --localize-symbol=OPENSSL_hexstr2buf        $SRC_PATH/third_party/lib/libgmssl.a
+cd $SRC_PATH
+cd third_party/gperftools/ && git checkout d9a5d38 && ./autogen.sh && ./configure --prefix=$SRC_PATH/third_party/ && make -j${nproc} && make install
+cd $SRC_PATH
+#cd third_party/secp256k1 && git checkout a660a49 && cmake -S . -B build_release -DSECP256K1_ENABLE_MODULE_RECOVERY=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_POLICY_VERSION_MINIMUM=3.5 -DCMAKE_INSTALL_PREFIX=$SRC_PATH/third_party/ && cd build_release && make -j${nproc} && make install
+cd third_party/secp256k1 && git checkout a660a49 && bash ./autogen.sh && ./configure --enable-module-ecdh --with-internal-keccak --disable-ecmult-static-precomputation --enable-module-recovery --enable-module-schnorrsig --prefix=$SRC_PATH/third_party/ && make -j${nproc} && make install
 
-# Libbls
-build_lib "$TP_PATH/lib/libdkgbls.a" "third_party/libbls" \
-    "cd deps && PARALLEL_COUNT=1 bash build.sh && cd .. && cmake -S . -B build_release -DUSE_ASM=False -DWITH_PROCPS=OFF -DLIBBLS_BUILD_TESTS=OFF -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$TP_PATH && cd build_release && make -j${nproc} install && \
-     mkdir -p $TP_PATH/include/libbls && cp -rnf ../third_party ../tools ../dkg ../bls $TP_PATH/include/libbls && cp -rnf ./libbls.a $TP_PATH/lib/libdkgbls.a"
+cd $SRC_PATH
+cd third_party/gtest &&  git submodule update --init && cmake -S . -B build_release -DCMAKE_POLICY_VERSION_MINIMUM=3.5 -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$SRC_PATH/third_party/ && cd build_release && make -j${nproc} && make install
+cd $SRC_PATH
+cd third_party/zstd &&  git submodule update --init && cmake -S . -B build_release -DCMAKE_POLICY_VERSION_MINIMUM=3.5 -DJSON_BuildTests=OFF -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$SRC_PATH/third_party/ && cd build_release && make -j${nproc} && make install
 
-# Protobuf
-build_lib "$TP_PATH/bin/protoc" "third_party/protobuf" \
-    "git checkout 48cb18e && ./autogen.sh && ./configure --disable-shared --enable-static CXXFLAGS='-fPIC -O3' --prefix=$TP_PATH && make -j${nproc} install"
+cd $SRC_PATH
+cd third_party/fmt &&  git submodule update --init && cmake -S . -B build_release -DCMAKE_POLICY_VERSION_MINIMUM=3.5 -DJSON_BuildTests=OFF -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$SRC_PATH/third_party/ && cd build_release && make -j${nproc} && make install
+cd $SRC_PATH
+cd third_party/pbc && make -f simple.make
+mkdir -p $SRC_PATH/third_party/include/pbc && cp -rnf ./include/* $SRC_PATH/third_party/include/pbc && cp -rnf ./lib*.a  $SRC_PATH/third_party/lib
 
-# Spdlog
-build_lib "$TP_PATH/lib/libspdlog.a" "third_party/spdlog" \
-    "git checkout . && git submodule update --init && cmake -S . -B build_release -DSPDLOG_ENABLE_SOURCE_LOC=ON -DWITH_TESTS=OFF -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$TP_PATH && cd build_release && make -j${nproc} install"
+cd $SRC_PATH
+cd third_party/json &&  git submodule update --init && cmake -S . -B build_release -DCMAKE_POLICY_VERSION_MINIMUM=3.5 -DJSON_BuildTests=OFF -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$SRC_PATH/third_party/ && cd build_release && make -j${nproc} && make install
 
-# RocksDB
-build_lib "$TP_PATH/lib/librocksdb.a" "third_party/rocksdb" \
-    "git checkout . && sed -i 's/-march=native//g' ./CMakeLists.txt && git submodule update --init && cmake -S . -B build_release -DWITH_TESTS=OFF -DPORTABLE=1 -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$TP_PATH && cd build_release && make -j${nproc} install"
+sed -i 's/private/public/g' $SRC_PATH/third_party/include/pbc/pbcxx.h
+sed -i 's/protected/public/g' $SRC_PATH/third_party/include/pbc/pbcxx.h
+cd $SRC_PATH
+cd third_party/cpppbc && git checkout . && sed -i 's/CXXFLAGS=/CXXFLAGS=-I\.\.\/include -L\.\.\/lib /g' ./Makefile && make -j8 libPBC.a
+mkdir -p $SRC_PATH/third_party/include/cpppbc && cp -rnf ./*.h $SRC_PATH/third_party/include/cpppbc
+cp -rnf ./lib*.a $SRC_PATH/third_party/lib
+cd $SRC_PATH
+cd third_party/clickhouse &&  git submodule update --init && cmake -S . -B build_release -DCMAKE_POLICY_VERSION_MINIMUM=3.5 -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$SRC_PATH/third_party/ && cd build_release && make -j${nproc} && make install
+sed -i 's/ciso646/version/g' ../contrib/absl/absl/base/options.h
+cp -rnf ../contrib/absl/absl $SRC_PATH/third_party/include/
+cp -rnf ../contrib/lz4/lz4 $SRC_PATH/third_party/include/
+cp -rnf ../contrib/zstd/zstd $SRC_PATH/third_party/include/
 
-# GmSSL
-build_lib "$TP_PATH/lib/libgmssl.a" "third_party/gmssl" \
-    "git checkout d655c06 && sed -i 's/-march=native//g' ./CMakeLists.txt && cmake -S . -B build_release -DBUILD_SHARED_LIBS=OFF -DENABLE_SM2_EXTS=on -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$TP_PATH && cd build_release && make -j${nproc} install && \
-     objcopy --localize-symbol=OPENSSL_hexchar2int --localize-symbol=OPENSSL_hexstr2buf $TP_PATH/lib/libgmssl.a"
 
-# Secp256k1
-build_lib "$TP_PATH/lib/libsecp256k1.a" "third_party/secp256k1" \
-    "git checkout a660a49 && bash ./autogen.sh && ./configure --enable-module-ecdh --enable-module-recovery --enable-module-schnorrsig --prefix=$TP_PATH && make -j${nproc} install"
+cd $SRC_PATH
+cd third_party/geolite2pp && git checkout . && sed -i 's/const auto iter/const auto\& iter/g' ./src-main/main.cpp &&  sed -i '11i\include_directories(SYSTEM '$SRC_PATH'/third_party/include/maxmind/)' CMakeLists.txt && cmake -S . -B build_release -DCMAKE_POLICY_VERSION_MINIMUM=3.5 -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$SRC_PATH/third_party/  -DCMAKE_PREFIX_PATH=$SRC_PATH/third_party/ -DCMAKE_INCLUDE_PATH=$SRC_PATH/third_party/include/maxmind/ && cd build_release && make -j${nproc} && make install
 
-# Clickhouse (Abseil/LZ4/Zstd)
-build_lib "$TP_PATH/include/absl" "third_party/clickhouse" \
-    "git submodule update --init && cmake -S . -B build_release -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$TP_PATH && cd build_release && make -j${nproc} install && \
-     cp -rnf ../contrib/absl/absl $TP_PATH/include/ && cp -rnf ../contrib/lz4/lz4 $TP_PATH/include/"
 
-# OpenSSL
-build_lib "$TP_PATH/lib/libssl.a" "third_party/openssl" \
-    "git checkout 7b371d8 && ./Configure --prefix=$TP_PATH && make -j${nproc} install"
+cd $SRC_PATH
+cd third_party/xxHash/ && make -j${nproc} && mkdir -p $SRC_PATH/third_party/include/xxHash && cp -rnf ./*.h $SRC_PATH/third_party/include/xxHash && cp -rnf cachedObjs/*/libxxhash.a $SRC_PATH/third_party/lib
 
-# OQS
-build_lib "$TP_PATH/lib/liboqs.a" "third_party/oqs" \
-    "git checkout 94b421e && cmake -S . -B build_release -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$TP_PATH && cd build_release && make -j${nproc} install"
+cd $SRC_PATH
+cd third_party/ethash && git checkout 83bd5ad && cmake -S . -B build_release -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DCMAKE_POLICY_VERSION_MINIMUM=3.5 -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$SRC_PATH/third_party/ && cd build_release && make -j${nproc} && mkdir -p $SRC_PATH/third_party/include/ethash && cp -rnf ../include/ethash/* $SRC_PATH/third_party/include/ethash && cp -rnf ./lib/keccak/libkeccak.a ./lib/ethash/libethash.a ./lib/global_context/libethash-global-context.a $SRC_PATH/third_party/lib
 
-# --- 3. Cleanup Shared Libs ---
-# Final step to ensure only static libs are used
+
+cd $SRC_PATH
+cd third_party/openssl/ && git checkout 7b371d8 && ./Configure --prefix=$SRC_PATH/third_party/ && make -j${nproc} && make install
+
+cd $SRC_PATH
+cd third_party/readerwriterqueue && git checkout 8b21766 && mkdir -p $SRC_PATH/third_party/include/readerwriterqueue && cp -rnf ./*.h $SRC_PATH/third_party/include/readerwriterqueue
+
+cd $SRC_PATH
+mkdir -p $SRC_PATH/third_party/include/boost
+cd third_party/boost/multiprecision && git checkout c48ae18 && cmake -S . -B build_release -DCMAKE_POLICY_VERSION_MINIMUM=3.5 -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$SRC_PATH/third_party/ && cd build_release && make -j${nproc} && make install
+
+cd $SRC_PATH
+cd third_party/oqs && git checkout 94b421e && cmake -S . -B build_release -DCMAKE_BUILD_TYPE=Release -DCMAKE_POLICY_VERSION_MINIMUM=3.5 -DCMAKE_INSTALL_PREFIX=$SRC_PATH/third_party/ && cd build_release && make -j8 && make install
+
+cd $SRC_PATH
+cd third_party/leveldb && git checkout 99b3c03 && git submodule init && git submodule update && cmake -S . -B build_release -DLEVELDB_BUILD_TESTS=OFF -DLEVELDB_BUILD_BENCHMARKS=OFF -DCMAKE_POLICY_VERSION_MINIMUM=3.5 -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$SRC_PATH/third_party/ && cd build_release && make -j8 && make install
+
+
+cd $SRC_PATH
+cd third_party/fmt && cmake -S . -B build_release -DCMAKE_POLICY_VERSION_MINIMUM=3.5 -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$SRC_PATH/third_party/ && cd build_release && make -j${nproc} && make install
+
+cd $SRC_PATH
+cd third_party/httplib && cp ./httplib.h $SRC_PATH/third_party/include/
+
 cd $SRC_PATH
 rm -rf third_party/lib/lib*.so* third_party/lib64/lib*.so*
-
-echo "All third_party dependencies are ready."
