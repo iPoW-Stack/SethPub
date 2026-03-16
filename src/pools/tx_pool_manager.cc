cb09fbe00e9f7ae09a3846c9fcf97ae21ff26bcc
tenondvpn <jhsx5679@gmail.com>
2026-03-16 17:15:06 +0800

    fix

--- src/pools/tx_pool_manager.cc
+++ src/pools/tx_pool_manager.cc
@@ -214,9 +214,9 @@ int TxPoolManager::TmpFirewallCheckMessage(const transport::MessagePtr& msg_ptr)
 
     auto leader = hotstuff_mgr_->is_other_leader(msg_ptr->address_info->pool_index());
     if (leader) {
-        auto dht = network::UniversalManager::Instance()->GetDht(network::kRootCongressNetworkId);
+        auto dht = network::UniversalManager::Instance()->GetUniversal(network::kRootCongressNetworkId);
         auto dht_vec = dht->readonly_hash_sort_dht();
-        auto node_it = std::find_if(dht_vec.begin(), dht_vec.end(), [&](const auto& item) {
+        auto node_it = std::find_if(dht_vec->begin(), dht_vec->end(), [&](const auto& item) {
             return item->id == leader->id;
         });
 
