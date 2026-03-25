9bcabb2dd88204147d6dd2abc361bd40d4cebe4c
tenondvpn <jhsx5679@gmail.com>
2026-03-25 17:40:21 +0800

    fix

--- src/consensus/hotstuff/hotstuff.cc
+++ src/consensus/hotstuff/hotstuff.cc
@@ -2241,6 +2241,8 @@ void Hotstuff::TryRecoverFromStuck(
         const transport::MessagePtr& msg_ptr, 
         bool has_user_tx, 
         bool has_system_tx) {
+    SETH_DEBUG("pool: %u, TryRecoverFromStuck, has_user_tx: %d, has_system_tx: %d",
+        pool_idx_, has_user_tx, has_system_tx);
     auto now_tm_ms = common::TimeUtils::TimestampMs();
     if (latest_qc_item_ptr_ && update_latest_view_tm_) {
         laste_vote_prev_view_tm_.Put(latest_qc_item_ptr_->view(), now_tm_ms);
