b4a889000c39d7d47eea01d70eeab33d8ddbee6a
tenondvpn <jhsx5679@gmail.com>
2026-03-30 19:17:07 +0800

    fix

--- simple_remote_bin.sh
+++ simple_remote_bin.sh
@@ -149,19 +149,21 @@ ln_all_nodes() {
         fi
 
         for ((shard=2; shard<=end_shard; shard++)); do
-            echo "/root/seths/s${shard}_${start_pos}/"
-            REMOTE_CMD="if [ -d \"/root/seths/s${shard}_${start_pos}/\" ]; then 
-                            rm -rf /root/seths/s${shard}_${start_pos}/seth && \
-                            rm -rf /root/seths/s${shard}_${start_pos}/log/* && \
-                            ln /root/pkg/seth /root/seths/s${shard}_${start_pos}/seth && \
-                            echo \"[Shard $shard] Updated\"; 
-                        else 
-                            echo \"[Shard $shard] Not exists, skipping\"; 
-                        fi"
-            sshpass -p "$PASSWORD" ssh -o ConnectTimeout=3 -o "StrictHostKeyChecking no" -o ServerAliveInterval=5 root@$ip "$REMOTE_CMD" &
-            if ((start_pos == 1)); then
-                sleep 3
-            fi
+            for ((i=1; i<=$each_nodes_count;i++)); do
+                echo "/root/seths/s${shard}_${start_pos}/"
+                REMOTE_CMD="if [ -d \"/root/seths/s${shard}_${start_pos}/\" ]; then 
+                                rm -rf /root/seths/s${shard}_${start_pos}/seth && \
+                                rm -rf /root/seths/s${shard}_${start_pos}/log/* && \
+                                ln /root/pkg/seth /root/seths/s${shard}_${start_pos}/seth && \
+                                echo \"[Shard $shard] Updated\"; 
+                            else 
+                                echo \"[Shard $shard] Not exists, skipping\"; 
+                            fi"
+                sshpass -p "$PASSWORD" ssh -o ConnectTimeout=3 -o "StrictHostKeyChecking no" -o ServerAliveInterval=5 root@$ip "$REMOTE_CMD" &
+                if ((start_pos == 1)); then
+                    sleep 3
+                fi
+            done
         done
         sleep 0.1
         start_pos=$(($start_pos+$start_nodes_count))
