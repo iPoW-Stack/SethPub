fb604f324e158b9e8d410375496914a439aebfc8
tenondvpn <jhsx5679@gmail.com>
2026-03-30 13:55:52 +0800

    fix

--- clipy/cli.py
+++ clipy/cli.py
@@ -237,10 +237,9 @@ def test_library():
     // SPDX-License-Identifier: MIT
     pragma solidity ^0.8.0;
 
-    library MathLib {
-        function add(uint256 a, uint256 b) public pure returns (uint256) {
-            return a + b;
-        }
+    library MathLib { 
+        function add(uint256 a, uint256 b) public pure returns (uint256) { return a+b; } 
+        function dec(uint256 a, uint256 b) public pure returns (uint256) { return a-b; } 
     }
 
     contract Calculator {
