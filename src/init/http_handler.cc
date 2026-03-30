94d77660286e6cebc89d5799a7f89f644185c63e
tenondvpn <jhsx5679@gmail.com>
2026-03-30 10:06:33 +0800

    fix

--- src/init/http_handler.cc
+++ src/init/http_handler.cc
@@ -387,6 +387,7 @@ static void QueryContract(const httplib::Request& req, httplib::Response& http_r
     zjc_host.contract_mgr_ = contract_mgr;
     zjc_host.my_address_ = contract_addr;
     zjc_host.tx_context_.block_gas_limit = prepayment;
+    zjc_host.view_block_chain_ = std::make_shared<view_block::ViewBlockChain>();
     // user caller prepayment 's gas
     uint64_t from_balance = prepayment;
     uint64_t to_balance = contract_addr_info->balance();
