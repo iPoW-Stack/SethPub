9fe3fdeadd13e5112013092f8df8e5811afed553
tenondvpn <jhsx5679@gmail.com>
2026-04-01 10:34:24 +0800

    fix:

--- src/pools/tx_utils.h
+++ src/pools/tx_utils.h
@@ -271,7 +271,22 @@ static inline std::string GetTxMessageHash(const pools::protobuf::TxMessage& tx_
         }
     }
 
-    SETH_DEBUG("get tx message hash: %s", common::Encode::HexEncode(message).c_str());
+    SETH_DEBUG("get tx message hash: %s, nonce: %lu, step: %d, pk: %s, to: %s, amount: %lu, gas limit: %lu, price: %lu, step: %lu, contract code: %s, input: %s, prepay: %lu, key: %s, value: %s", 
+        common::Encode::HexEncode(message).c_str(),
+        nonce,
+        tx_info.step(),
+        common::Encode::HexEncode(tx_info.pubkey()).c_str(),
+        common::Encode::HexEncode(tx_info.to()).c_str(),
+        amount,
+        gas_limit,
+        gas_price,
+        step,
+        tx_info.has_contract_code() ? common::Encode::HexEncode(tx_info.contract_code()).c_str() : "",
+        tx_info.has_contract_input() ? common::Encode::HexEncode(tx_info.contract_input()).c_str() : "",
+        tx_info.has_contract_prepayment() ? tx_info.contract_prepayment() : 0,
+        tx_info.has_key() ? common::Encode::HexEncode(tx_info.key()).c_str() : "",
+        tx_info.has_value() ? common::Encode::HexEncode(tx_info.value()).c_str() : ""
+        common::Encode::HexEncode(message).c_str());
     return common::Hash::keccak256(message);
 }
 
