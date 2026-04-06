5cda8bad16988e7bc962068e9a86c6c4a0555a66
jhsx5679@gmail.com <xl@xldeMacBook-Air.local>
2026-04-06 12:36:39 +0800

    fix

--- clipy/seth3.py
+++ clipy/seth3.py
@@ -418,11 +418,11 @@ def oqs_sign_test():
     w3 = SethWeb3Mock(IP, PORT)
     MY_OQS = w3.client.get_oqs_address(OQS_PK)
 
-    test_oqs_transfer(w3, MY_OQS, OQS_KEY, OQS_PK)
-    test_oqs_contract_deploy_and_call(w3, MY_OQS, OQS_KEY, OQS_PK)
-    test_oqs_library_with_contract(w3, MY_OQS, OQS_KEY, OQS_PK)
+    # test_oqs_transfer(w3, MY_OQS, OQS_KEY, OQS_PK)
+    # test_oqs_contract_deploy_and_call(w3, MY_OQS, OQS_KEY, OQS_PK)
+    # test_oqs_library_with_contract(w3, MY_OQS, OQS_KEY, OQS_PK)
     test_oqs_contract_prefund_flow(w3, MY_OQS, OQS_KEY, OQS_PK)
 
 if __name__ == "__main__":
     ecdsa_sign_test()
-    # oqs_sign_test()
+    oqs_sign_test()
