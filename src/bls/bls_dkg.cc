5019f139f95ea9ebd4d7563ac9c488a313ebdd2e
tenondvpn <jhsx5679@gmail.com>
2026-04-09 20:48:07 +0800

    fix

--- src/bls/bls_dkg.cc
+++ src/bls/bls_dkg.cc
@@ -1034,7 +1034,7 @@ void BlsDkg::BroadcastFinish(const common::Bitmap& bitmap) {
         common::Encode::HexEncode(sign_hash).c_str(), 
         common_pk->x_c0().c_str(), msg_ptr->header.hash64());
     network::Route::Instance()->Send(msg_ptr);
-    transport::TcpTransport::Instance()->SendToLocal(msg_ptr);
+    transport::TcpTransport::Instance()->AddLocalMessage(msg_ptr);
     // if (common::GlobalInfo::Instance()->network_id() == network::kRootCongressNetworkId) {
     //     bls_mgr_->HandleMessage(msg_ptr);
     // }
