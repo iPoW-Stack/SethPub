aae18a77947de063255fb4230624fb102b66e6eb
tenondvpn <jhsx5679@gmail.com>
2026-04-13 16:59:25 +0800

    fix

--- src/consensus/hotstuff/hotstuff.cc
+++ src/consensus/hotstuff/hotstuff.cc
@@ -282,7 +282,6 @@ Status Hotstuff::Propose(
         }
 
         auto broadcast = tmp_msg_ptr->header.mutable_broadcast();
-        auto brd_param = header.mutable_broadcast();
         broadcast::SetDefaultBroadcastParam(broadcast);        auto* hotstuff_msg = tmp_msg_ptr->header.mutable_hotstuff();
         if (tc != nullptr) {
             auto* pb_pro_msg = hotstuff_msg->mutable_pro_msg();
