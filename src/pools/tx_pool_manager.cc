59d4e8928299c8a4f7558d39d97ad37997ac94c3
jhsx5679@gmail.com <xl@xldeMacBook-Air.local>
2026-04-20 22:58:44 +0800

    fix

--- src/pools/tx_pool_manager.cc
+++ src/pools/tx_pool_manager.cc
@@ -226,11 +226,11 @@ int TxPoolManager::TmpFirewallCheckMessage(const transport::MessagePtr& msg_ptr)
     auto leader = hotstuff_mgr_->is_other_leader(msg_ptr->address_info->pool_index());
     if (leader) {
         auto network_id = network::GetLocalConsensusNetworkId();
-        auto dht = network::DhtManager::Instance()->GetUniversal(network_id);
+        auto dht = network::DhtManager::Instance()->GetDht(network_id);
         if (dht == nullptr) {
             dht = network::UniversalManager::Instance()->GetUniversal(network::kNodeNetworkId);
         }
-        
+
         auto dht_vec = dht->readonly_hash_sort_dht();
         auto node_it = std::find_if(dht_vec->begin(), dht_vec->end(), [&](const auto& item) {
             return item->id == leader->id;
