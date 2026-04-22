8cdc942f1b67cc2f6b2088683f6f9e863e9d55a1
jhsx5679@gmail.com <xl@xldeMacBook-Air.local>
2026-04-23 00:09:04 +0800

    fix

--- clipy/test_contract_chain_demo.py
+++ clipy/test_contract_chain_demo.py
@@ -536,7 +536,7 @@ def test_contract_chain_same_shard_pool(w3, MY, KEY):
         
         new_key, new_addr, new_info = create_and_wait_for_address(
             w3, user1_key, target_shard, target_pool, 
-            initial_balance=9000000000, max_wait=60
+            initial_balance=5000000000, max_wait=60
         )
         
         if new_key and new_info:
@@ -595,7 +595,7 @@ def test_contract_chain_same_shard_pool(w3, MY, KEY):
         
         new_key, new_addr, new_info = create_and_wait_for_address(
             w3, user1_key, target_shard, target_pool,
-            initial_balance=9000000000, max_wait=60
+            initial_balance=5000000000, max_wait=60
         )
         
         if new_key and new_info:
