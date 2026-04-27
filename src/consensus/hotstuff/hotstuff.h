fa8831f5051380bb71d850f923ae840472f8f7b5
tenondvpn <jhsx5679@gmail.com>
2026-04-27 16:40:10 +0800

    fix

--- src/consensus/hotstuff/hotstuff.h
+++ src/consensus/hotstuff/hotstuff.h
@@ -376,7 +376,6 @@ private:
             
             // *out_view += 1;
             
-            if (debug)
             SETH_DEBUG("pool: %u, leader_latest_qc view: %lu is equal with high view block qc view: %lu, "
                 "high_view_block->qc().elect_height(): %lu, latest_elect_height_: %lu, out view: %lu, "
                 "last_stable_leader_member_index_: %u, new_leader_idx: %u, leader_latest_qc.leader_idx(): %u",
@@ -386,7 +385,7 @@ private:
                 last_stable_leader_member_index_.load(),
                 new_leader_idx,
                 leader_latest_qc.leader_idx());
-            pool_tx_leader_.store((*members)[new_leader_idx % members->size()]);
+            pool_tx_leader_.store((*members)[last_stable_leader_member_index_ % members->size()]);
             return (*members)[last_stable_leader_member_index_ % members->size()];
         }
 
