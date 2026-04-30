2efb685311f507fc45358ef6744439aa779d2433
jhsx5679@gmail.com <xl@xldeMacBook-Air.local>
2026-04-30 23:54:05 +0800

    fix

--- src/consensus/hotstuff/view_block_chain.cc
+++ src/consensus/hotstuff/view_block_chain.cc
@@ -344,6 +344,9 @@ std::shared_ptr<ViewBlockInfo> ViewBlockChain::GetViewBlockWithHash(const HashSt
     // // CheckThreadIdValid();
     std::shared_ptr<ViewBlockInfo> view_block_info_ptr;
     while (remove && cached_block_queue_.pop(&view_block_info_ptr)) {
+        if (!view_block_info_ptr || !view_block_info_ptr->view_block) {
+            continue;
+        }
         cached_block_map_[view_block_info_ptr->view_block->qc().view_block_hash()] = view_block_info_ptr;
         cached_pri_queue_.push(view_block_info_ptr);
         cached_view_with_blocks_[view_block_info_ptr->view_block->qc().view()].push_back(view_block_info_ptr);
