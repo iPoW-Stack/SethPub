a124f7bcf3239edc93f873b0fea1896009276360
jhsx5679@gmail.com <xl@xldeMacBook-Air.local>
2026-05-10 02:41:29 +0800

    fix

--- src/consensus/hotstuff/hotstuff.cc
+++ src/consensus/hotstuff/hotstuff.cc
@@ -429,9 +429,10 @@ Status Hotstuff::Propose(
         pb_pro_msg->view_item().block_info().timestamp());
     
 
-    if (msg_ptr->header.hotstuff().pro_msg().tx_propose().txs_size() == 0) {
-        auto latest_view_block_ptr = view_block_chain()->HighViewBlock();
-        if (latest_view_block_ptr->block_info().tx_list_size() == 0) {
+    if (msg_ptr->header.hotstuff().pro_msg().tx_propose().txs_size() == 0 && 
+            latest_qc_item_ptr_ && latest_qc_item_ptr_->view() > 0) {
+        auto latest_view_block_ptr = view_block_chain()->Get(latest_qc_item_ptr_->view_block_hash());
+        if (latest_view_block_ptr && latest_view_block_ptr->block_info().tx_list_size() == 0) {
             SETH_DEBUG("pool: %d, set latest_leader_propose_message_ = nullptr, "
                 "latest_view_block_ptr->block_info().tx_list_size() == 0", pool_idx_);
             latest_leader_propose_message_ = nullptr;
